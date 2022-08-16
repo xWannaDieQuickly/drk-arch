82b1dcb5d2c610af96d728c67140f14bdf3fd335
Sebastian <s.joist@gmx.net>
2022-08-16 19:03:13 +0200

    Finalize

--- backup.sh
+++ backup.sh
@@ -23,10 +23,10 @@ full_backup() {
 
 	# Create Full-Backup
 	clean_dir_name=$(echo $1 | tr "/" "_")
-	tar -czf /tmp/${2}${clean_dir_name}_full_backup.tar.gz $1
+	tar -czf /tmp/backups/${2}${clean_dir_name}_backup.tar.gz $1
 
 	# Safe the date
-	echo $2 >/tmp/full_backup$clean_dir_name
+	echo $2 >/tmp/backups/last_backup$clean_dir_name
 
 	# Delete BackUp after it was send to the server
 	# rm /tmp/${2}${clean_dir_name}_full_backup.tar.gz
@@ -49,13 +49,15 @@ incremental_backup() {
 
 	# Check if a full Back-Up of the given directory has already been created
 	# If no, create full Back-Up instead of incremental Back-Up
-	if [ ! -f "/tmp/${2}${clean_dir_name}_full_backup.tar.gz" ]; then
+	if [ ! -f "/tmp/backups/${2}${clean_dir_name}_backup.tar.gz" ]; then
 		full_backup $1 $2
 		return
 	fi
 
+	last_full_backup=$(cat /tmp/backups/last_backup$clean_dir_name)
+
 	# Create new incremental BackUp
-	tar -cz --after-date=$last_full_backup -f /tmp/${2}${clean_dir_name}_inc_backup.tar.gz $1
+	tar -cz --newer-mtime=$(echo $last_full_backup | tr "_" "-") -f /tmp/backups/${2}${clean_dir_name}_backup.tar.gz $1
 
 	# Delete BackUp after it was send to the server
 	# rm /tmp/${2}${clean_dir_name}_inc_backup.tar.gz
@@ -77,10 +79,11 @@ save_to_disk() {
 
 	clean_dir_name=$(echo $1 | tr "/" "_")
 
-	tar -czf ${3}${2}${clean_dir_name}_full_backup.tar.gz $1
+	tar -czf ${3}${2}${clean_dir_name}_backup.tar.gz $1
 }
 
 # Check if any options are entered
+# Output help for the user
 if [ $# -eq "0" ]; then
 	echo "Please enter a valid Option:"
 	echo "-d [/path/to/disk] to save the backup to a disk"
@@ -90,32 +93,33 @@ fi
 
 # Current date
 cur_date=$(date +%Y_%m_%d)
+user=$(whoami)
 
 while getopts "d:l" opt; do
 	case $opt in
 	d) # Save the backup to a disk
-		save_to_disk "/home" $cur_date $OPTARG
+		save_to_disk "/home/"$user $cur_date $OPTARG
 		exit
 		;;
 	l) # Save the backup locally
 
 		# Check if a date for the last backup is stored
-		if [ ! -f /tmp/full_backup_home ]; then
+		if [ ! -f /tmp/backups/last_backup_home_$user ]; then
 			echo -e "You started the first Backup"
-			full_backup "/home" $cur_date
+			full_backup "/home/"$user $cur_date
 			exit
 		fi
 
-		last_full_backup=$(cat /tmp/full_backup_home)
+		last_full_backup=$(cat /tmp/backups/last_backup_home_$user)
 
 		# Check if the last full back up was made this week
 		# if yes do an incremental backup
 		if [ $(date +%U -d $(echo $last_full_backup | tr "_" "-")) != $(date +%U -d $(echo $cur_date | tr "_" "-")) ]; then
 			echo "The last full backup was last week"
-			full_backup "/home" $cur_date
+			full_backup "/home/"$user $cur_date
 		else
 			echo "The last full backup was this week"
-			incremental_backup "/home" $cur_date
+			incremental_backup "/home/"$user $cur_date
 		fi
 		;;
 	\?) # No valid option was given
