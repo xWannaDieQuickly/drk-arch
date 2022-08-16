9ccb5e62792bcf47e4b1a04b253bcfecf617f944
Sebastian <s.joist@gmx.net>
2022-08-16 17:08:25 +0200

    Final Version

--- backup.sh
+++ backup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # Program: backup.sh
-# Call: ./backup.sh
+# Call: ./backup.sh [option]
+#		-d = save the backup on a disk / harddrive
+#		-l = save the backup locally
 # Description: File backup program
 # Author: S. Joist
 # Version: 1.0
@@ -15,7 +17,7 @@ full_backup() {
 
 	# Check if directory exists
 	if [ ! -d "$1" ]; then
-		echo -e "$1 does not exist. Please enter a valid directory (/path/to/directory).\n\n"
+		echo -e "$1 does not exist. Please enter a valid directory (/path/to/directory).\n"
 		return
 	fi
 
@@ -26,14 +28,8 @@ full_backup() {
 	# Safe the date
 	echo $2 >/tmp/full_backup$clean_dir_name
 
-	# Send archive to Server
-	# scp /temp/full_backup_$2.tar.gz student1@192.168.10.1: ${2}${clean_dir_name}_full_backup.tar.gz
-
-	# Log BackUp events
-	echo -e "Successfully created a new Full BackUp of $1\n" >>/tmp/full_backup${clean_dir_name}.log
-
 	# Delete BackUp after it was send to the server
-	rm /tmp/${2}${clean_dir_name}_full_backup.tar.gz
+	# rm /tmp/${2}${clean_dir_name}_full_backup.tar.gz
 
 }
 
@@ -45,36 +41,87 @@ incremental_backup() {
 
 	# Check if directory exists
 	if [ ! -d "$1" ]; then
-		echo -e "$1 does not exist. Please enter a valid directory (/path/to/directory).\n\n"
+		echo -e "$1 does not exist. Please enter a valid directory (/path/to/directory).\n"
 		return
 	fi
 
 	clean_dir_name=$(echo $1 | tr "/" "_")
 
-	last_full_backup=$(cat /tmp/full_backup$clean_dir_name)
-
-	echo $last_full_backup
+	# Check if a full Back-Up of the given directory has already been created
+	# If no, create full Back-Up instead of incremental Back-Up
+	if [ ! -f "/tmp/${2}${clean_dir_name}_full_backup.tar.gz" ]; then
+		full_backup $1 $2
+		return
+	fi
 
 	# Create new incremental BackUp
 	tar -cz --after-date=$last_full_backup -f /tmp/${2}${clean_dir_name}_inc_backup.tar.gz $1
 
-	# Send archive to Server
-	# scp /tmp/${2}${clean_dir_name}_inc_backup.tar.gz student1@192.168.10.1: ${2}${clean_dir_name}_inc_backup.tar.gz
-
-	# Log BackUp events
-	echo -e "Successfully created a new Full BackUp of $1\n" >>/tmp/inc_backup${clean_dir_name}.log
-
 	# Delete BackUp after it was send to the server
-	rm /tmp/${2}${clean_dir_name}_inc_backup.tar.gz
+	# rm /tmp/${2}${clean_dir_name}_inc_backup.tar.gz
 
 }
 
-main() {
-	# Current date
-	# Used to name the BackUp file
-	cur_date=$(date +%Y_%m_%d)
+# Save the Back-Up as Archive to a disk
+# Args:
+# 1: Directory
+# 2: Current date
+# 3: Disk / Harddrive
+save_to_disk() {
 
-	echo 
+	# Check if disk exists
+	if [ ! -d "$3" ]; then
+		echo -e "$3 does not exist. Please enter a valid disc (/path/to/disk).\n"
+		return
+	fi
+
+	clean_dir_name=$(echo $1 | tr "/" "_")
+
+	tar -czf ${3}${2}${clean_dir_name}_full_backup.tar.gz $1
 }
 
-main
+# Check if any options are entered
+if [ $# -eq "0" ]; then
+	echo "Please enter a valid Option:"
+	echo "-d [/path/to/disk] to save the backup to a disk"
+	echo "-l to save the backup locally"
+	exit
+fi
+
+# Current date
+cur_date=$(date +%Y_%m_%d)
+
+while getopts "d:l" opt; do
+	case $opt in
+	d) # Save the backup to a disk
+		save_to_disk "/home" $cur_date $OPTARG
+		exit
+		;;
+	l) # Save the backup locally
+
+		# Check if a date for the last backup is stored
+		if [ ! -f /tmp/full_backup_home ]; then
+			echo -e "You started the first Backup"
+			full_backup "/home" $cur_date
+			exit
+		fi
+
+		last_full_backup=$(cat /tmp/full_backup_home)
+
+		# Check if the last full back up was made this week
+		# if yes do an incremental backup
+		if [ $(date +%U -d $(echo $last_full_backup | tr "_" "-")) != $(date +%U -d $(echo $cur_date | tr "_" "-")) ]; then
+			echo "The last full backup was last week"
+			full_backup "/home" $cur_date
+		else
+			echo "The last full backup was this week"
+			incremental_backup "/home" $cur_date
+		fi
+		;;
+	\?) # No valid option was given
+		echo "Please enter a valid Option:"
+		echo "-d [/path/to/disk] to save the backup to a disk"
+		echo "-l to save the backup locally"
+		;;
+	esac
+done
