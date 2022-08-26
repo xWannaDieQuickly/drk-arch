29be615255f44675694ee0eb6e2a78da365b820a
Sebastian Joist <s.joist@gmx.net>
2022-08-26 10:12:53 +0200

    MAC

--- hw_info.py
+++ hw_info.py
@@ -1,6 +1,7 @@
 import platform
 import getmac
-import os, sys
+import os
+import sys
 import subprocess
 
 # Computer network name
@@ -20,8 +21,14 @@ print(f"Operating system version: {platform.version()}")
 # MAC Adress
 print(f"MAC Adress: {getmac.get_mac_address()}")
 
+string = "STRING"
+
 booted = "UEFI" if os.path.exists("/sys/firmware/efi") else "BIOS"
 print("The system booted with %s" % booted)
 
+js = {"AB": "CD",
+      "CD": "AB"}
 
-subprocess.run(["echo", "TEST123"], check=True, text=True)
+
+subprocess.run(["echo", "myTest", string, js], check=True, text=True)
+#subprocess.run(["archinstall", "--config" "config.json", "--dry-run"], check=True, text=True)
