0c79a818897f015e5be381b126557d5b11dc055f
Sebastian Joist <s.joist@gmx.net>
2022-08-27 17:07:01 +0200

    m

--- hw_info.py
+++ hw_info.py
@@ -97,10 +97,8 @@ def main():
     # lspci
     lst = subprocess.run(["lspci"], check=True,
                          text=True, capture_output=True).stdout.splitlines()
-    print(len(lst))
-    print(type(lst))
-    print(repr(subprocess.run(["lspci"], check=True,
-                              text=True, capture_output=True).stdout))
+    for e in lst:
+        print(e)
     # hwInfo = get_hw()
     # print(json.dumps(hwInfo, indent=4))
 
