cefcaf5bc24d85eaae56ac39e7c7cadb2022bce3
Sebastian Joist <s.joist@gmx.net>
2022-08-27 22:13:17 +0200

    m

--- pre.zsh
+++ pre.zsh
@@ -1,2 +1,5 @@
 #!/bin/zsh
-archinstall --config BackUp/config.json --creds BackUp/creds.json --disk_layouts BackUp/disk-layouts.json
+loadkeys de-latin1
+pacman -Sy python-pip archinstall --noconfirm
+pip install getmac psutil --no-input
+#archinstall --config BackUp/config.json --creds BackUp/creds.json --disk_layouts BackUp/disk-layouts.json
