91fb0e2a5ea6e7e37961449bbf3f03325fef6a71
Sebastian Joist <s.joist@gmx.net>
2022-09-18 13:05:53 +0200

    m

--- pre.zsh
+++ pre.zsh
@@ -1,5 +1,12 @@
 #!/bin/zsh
-loadkeys de-latin1
-pacman -Sy python-pip archinstall --noconfirm
+pacman -S git python-pip --noconfirm
+pip uninstall archinstall
+git clone https://github.com/archlinux/archinstall
+pip install setuptools
+cd archinstall
+pip install setuptools --no-input
+python setup.py install
 pip install getmac psutil --no-input
+chmod +x BackUp/hw-info.py
+python BackUp/hw-info.py
 #archinstall --config BackUp/config.json --creds BackUp/creds.json --disk_layouts BackUp/disk-layouts.json
