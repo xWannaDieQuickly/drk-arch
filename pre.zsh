d0d42344360b69f55c9f5803cd80a88f9929ff55
Sebastian Joist <s.joist@gmx.net>
2022-10-02 16:43:46 +0200

    m

--- pre.zsh
+++ pre.zsh
@@ -1,10 +1,10 @@
 #!/bin/zsh
 yes | pacman -S git python-pip
 yes | pip uninstall archinstall
-#git clone https://github.com/archlinux/archinstall
-#pip install setuptools getmac psutil --no-input
-#cd archinstall
-#python setup.py install
-#cd
-#python BackUp/automated_install.py
-#archinstall --config config.json --creds creds.json --disk_layouts disk-layouts.json --dry-runüp
+git clone https://github.com/archlinux/archinstall
+yes | pip install setuptools getmac psutil
+cd archinstall
+python setup.py install
+cd ../BackUp
+python automated_install.py
+archinstall --config config.json --creds creds.json --disk_layouts disk-layouts.json --dry-runüp
