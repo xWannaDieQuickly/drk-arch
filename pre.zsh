d7075a48e4f1267d5030f944f21d5c7d3a789a51
Sebastian Joist <s.joist@gmx.net>
2022-10-02 16:44:58 +0200

    m

--- pre.zsh
+++ pre.zsh
@@ -3,7 +3,7 @@ yes | pacman -S git python-pip
 yes | pip uninstall archinstall
 git clone https://github.com/archlinux/archinstall
 yes | pip install setuptools getmac psutil
-cd archinstall
+cd archinstall /root
 python setup.py install
 cd ../BackUp
 python automated_install.py
