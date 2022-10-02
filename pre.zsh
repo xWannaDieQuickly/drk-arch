3af9c5c3e01d552f7ce6d6828977b8e8b2e03d5b
Sebastian Joist <s.joist@gmx.net>
2022-10-02 17:29:04 +0200

    m

--- pre.zsh
+++ pre.zsh
@@ -5,6 +5,6 @@ git clone https://github.com/archlinux/archinstall
 yes | pip install setuptools getmac psutil
 cd archinstall
 python setup.py install
-cd ../BackUp
-python automated_install.py
+cd ../backup
+python cd automated_install.py
 archinstall --creds creds.json --disk_layouts disk-layouts.json 2> errors.txt
