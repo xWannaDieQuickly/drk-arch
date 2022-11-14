5dc755bd58a6bfc066e25782b04cd14da545cac2
Sebastian Joist <s.joist@gmx.net>
2022-11-14 15:13:19 +0100

    m

---
+++ setup.py
@@ -0,0 +1,7 @@
+# TODO: Move Desktop Applications to ~/.local/applications
+# TODO: Edit environment variables -> ~/.config/environent.d/variables.conf
+# TODO: Edit .desktop files | Add "NoDisplay=true" if app should not be shown
+# TODO: Move dconf-files to /etc/dconf/ -> Update dconf
+# TODO: Move Background image to /usr/local/share/background
+# TODO: Install Teamviewer from aur
+# TODO: Install Citrix from aur
