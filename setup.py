05c72c76a979c5341253bfe4e4a4e22c6563d3b5
Sebastian Joist <s.joist@gmx.net>
2022-11-15 23:16:42 +0100

    m

--- setup.py
+++ setup.py
@@ -1,25 +1,42 @@
 import shutil
 import os
+import subprocess
+from git import Repo
 
 
 # TODO: Move Background image to /usr/local/share/background
 # TODO: Install Teamviewer from aur
 # TODO: Install Citrix from aur
 # TODO: Install plymouth
+# TODO: Autorun teamviewer
 
 
 # Directories
 user_home_dir = '/home/mitarbeiter/'
-temp_dir = '/'
+admin_home_dir = '/home/admin/'
+temp_dwn_dir = '/temp/setup'
+github = 'https://www.github.com/xannadiequickly/backup'
+
 
 # TODO: Edit environment variables -> ~/.config/environent.d/variables.conf
-
-
 def create_env_var():
     # Check if file for environment variable exists
     # Else create new one
-    if not os.path.exists(f'{user_home_dir}.config/environment.d/variable.conf'):
-        shutil.copytree(f'{temp_dir}environment.d')
+    if not os.path.exists(f'{user_home_dir}.config/environment.d'):
+        shutil.copytree(f'{admin_home_dir}environment.d',
+                        f'{user_home_dir}.config/environment.d')
+
+
+# TODO: Move dconf-files to /etc/dconf/ -> Update dconf
+def setup_dconf():
+    shutil.rmtree('/etc/dconf')
+    shutil.copytree(f'{admin_home_dir}dconf', '/etc/')
+
+
+# TODO: Load grub.cfg
+def load_grub_cfg():
+    os.remove('/etc/default/grub')
+    shutil.copyfile(f'{temp_dwn_dir}grub', '/etc/default/')
 
 
 # TODO: Move Desktop Applications to ~/.local/applications
@@ -41,17 +58,26 @@ def setup_desktop_apps():
                 f.close()
 
 
-# TODO: Move dconf-files to /etc/dconf/ -> Update dconf
-def setup_dconf():
-    shutil.rmtree('/etc/dconf')
-    shutil.copytree(f'{temp_dir}dconf', 'etc/')
-    print()
+# def install_aur():
+#     for pkg in ['teamviewer', 'icaclient']:
+#         Repo.clone_from(
+#             f'https://aur.archlinux.org/{pkg}', admin_home_dir)
+#         subprocess.run([f"cd", admin_home_dir, pkg], check=True, text=True)
+#         subprocess.run(["sudo -u admin makepkg -si"], check=True, text=True)
+#         subprocess.run(["cd"], check=True, text=True)
+#         shutil.rmtree(f'{admin_home_dir}{pkg}')
+#         # systemctl enable
 
 
 def main():
+    os.mkdir('(/temp/setup')
+    Repo.clone_from(
+        github, temp_dwn_dir)
     create_env_var()
     setup_dconf()
     setup_desktop_apps()
+
+# sudo -u admin [cmd]
 
 
 main()
