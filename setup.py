e2615b15a31eb395c5ad87cf292fdc5f5dd01217
Sebastian Joist <s.joist@gmx.net>
2022-11-22 17:12:38 +0100

    m

--- setup.py
+++ setup.py
@@ -35,11 +35,11 @@ def setup_dconf():
 
         os.makedirs('/etc/dconf/profile/')
         os.makedirs('/etc/dconf/db/mitarbeiter.d/locks/')
-        shutil.copy(f'{temp_dwn_dir}dconf/profile/mitarbeiter.d',
+        shutil.copy(f'{temp_dwn_dir}dconf/profile/mitarbeiter',
                     '/etc/dconf/profile/')
         shutil.copy(f'{temp_dwn_dir}dconf/db/mitarbeiter.d/00-lockdown',
                     '/etc/dconf/db/mitarbeiter.d/')
-        shutil.copy(f'{temp_dwn_dir}dconf/db/01-background',
+        shutil.copy(f'{temp_dwn_dir}dconf/db/mitarbeiter.d/01-background',
                     '/etc/dconf/db/mitarbeiter.d/')
         shutil.copy(f'{temp_dwn_dir}dconf/db/mitarbeiter.d/locks/01-background',
                     '/etc/dconf/db/mitarbeiter.d/locks/')
